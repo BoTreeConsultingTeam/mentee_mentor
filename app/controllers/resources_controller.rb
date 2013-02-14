52ee1e6132221e4421c6f2eb2267ebc9138b7017
dilip <ddprajapati@gmail.com>
2013-02-15 01:07:27 +0530

    modify resource form partial base on coding standard
    - typo mistake in _form.html.hml extra space before _

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,13 +1,21 @@
 class ResourcesController < ApplicationController
 
 	before_filter :require_user
-
-	# GET /users/:user_id/mquests
+	
+	# GET /users/:user_id/resources
   def index
+  	# Fetch all users, except current logged in user.This is to allow a user to
+    # follow another user in the system
+    @users = User.where("id != ?", current_user.id)
   end
   
   def create
-  		#@list = current_user.resources.create(params[:resource])
+  		resource = Resource.create(params[:resource])
+      if (!resource.nil?) 
+        UserResource.create({:resource_id => resource.id,:user_id => current_user.id})
+      else
+        Rails.logger.debug "Resource errors found"
+      end
   end
 
 end
