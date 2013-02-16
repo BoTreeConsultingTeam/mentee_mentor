97a1b7d3b58b43b57c722e2a10cd42246de673ee
dilip <ddprajapati@gmail.com>
2013-02-16 22:07:45 +0530

    resource added into mbord
    - issue still exit for undefined method `resources_path'

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,20 +2,10 @@ class ResourcesController < ApplicationController
 
 	before_filter :require_user
 	
-	# GET /users/:user_id/resources
-  def index
-  	# Fetch all users, except current logged in user.This is to allow a user to
-    # follow another user in the system
-    @users = User.where("id != ?", current_user.id)
-  end
-  
-  def create
-  		resource = Resource.create(params[:resource])
-      if (!resource.nil?) 
-        UserResource.create({:resource_id => resource.id,:user_id => current_user.id})
-      else
-        Rails.logger.debug "Resource errors found"
-      end
+	def create      
+  	resource = Resource.create(params[:resource])
+    current_user.resources << resource
+    #current_user.resources.create(params[:resource])
   end
 
 end
