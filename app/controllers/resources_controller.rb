32ca1376e3877f136f44e1a02aa8dac4b7817bc2
dilip <ddprajapati@gmail.com>
2013-02-20 00:36:07 +0530

    resource form partial issue fixed and now resource content added properly
     - form_for issue fixed as mention by jignesh.
     - added error message text if resource not properly inserted.
     - redirected to mbord after record succesfully inserted.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,11 +1,17 @@
 class ResourcesController < ApplicationController
 
 	before_filter :require_user
-	
-	def create      
-  	resource = Resource.create(params[:resource])
-    current_user.resources << resource
-    #current_user.resources.create(params[:resource])
+
+    #POST /users/:id/resources
+	def create
+      user_resource_obj = current_user.resources.create(params[:resource])
+      if user_resource_obj.nil?
+        message = t('user.resource.errors.failure')
+      else
+        message = t('user.resource.errors.success')
+      end
+      Rails.logger.debug message
+      redirect_to mboard_user_path(current_user.id), flash: { error: message }
   end
 
 end
