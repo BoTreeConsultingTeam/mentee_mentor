21e13c9e4f72c3d9fd23b62435aeb6fdcc957183
Mahesh Gajera <mahesh.gajera2012@gmail.com>
2013-02-20 13:29:37 -0500

    Replacing JQuery UI components to datepicker only          Added minified and only required JQuery components          Updated Model error views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui.js
+//= require jquery-ui-custom.min.js
 //= require application
 //= require custom
 //= require_tree .
--- app/models/education.rb
+++ app/models/education.rb
@@ -2,5 +2,5 @@ class Education < ActiveRecord::Base
   attr_accessible :degree, :from_date, :school, :study_field, :to_date
   
   belongs_to :profile
-
+  
 end
