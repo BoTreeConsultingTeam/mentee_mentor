2f9575eaa4a49bd576806ad71253dd5fea7c6be3
Jignesh Gohel <jignesh.gohel@botreeconsulting.com>
2013-02-24 18:34:30 +0530

    Branch: development
        Fixed the migration ChangeBirthdayTypeToDateInProfiles which on Heroku when
        ran rake db:migrate caused following error:
    
          Migrating to ChangeBirthdayTypeToDateInProfiles (20130222191352)
          ==  ChangeBirthdayTypeToDateInProfiles: migrating =============================
          -- change_table(:profiles)
          rake aborted!
          An error has occurred, this and all later migrations canceled:
    
          PG::Error: ERROR:  column "birthday" cannot be cast to type date
          : ALTER TABLE "profiles" ALTER COLUMN "birthday" TYPE date
    
    ============= Updated
    
        /db/migrate/20130222191352_change_birthday_type_to_date_in_profiles.rb
          - Changed migration's up method implementation with reference from:
              http://stackoverflow.com/questions/3075920/how-do-i-change-column-type-in-heroku

--- db/migrate/20130222191352_change_birthday_type_to_date_in_profiles.rb
+++ db/migrate/20130222191352_change_birthday_type_to_date_in_profiles.rb
@@ -1,8 +1,11 @@
 class ChangeBirthdayTypeToDateInProfiles < ActiveRecord::Migration
   def up
-    change_table :profiles do |t|
-      t.change :birthday, :date
-    end
+    rename_column :profiles, :birthday, :birthday_entered_string
+    add_column :profiles, :birthday, :date
+
+    Profile.reset_column_information
+    Profile.find_each { |profile| profile.update_attribute(:birthday, profile.birthday_entered_string) }
+    remove_column :profiles, :birthday_entered_string
   end
 
   def down
